e9b9dc5a8658667739e204f6011f367e6d42ab83
NinaAvetisyan37 <nin.avetisyan.99@gmail.com>
2020-09-30 21:53:33 +0400

    X O game

---
+++ xo.js
@@ -0,0 +1,83 @@
+let turn=document.getElementById("turn");
+let boxes=document.querySelectorAll("#main div");
+let xo=0;
+
+function selectWinnerBoxes(b1,b2,b3){
+	b1.classList.add("win"); 
+	b2.classList.add("win"); 
+	b3.classList.add("win"); 
+	turn.innerHTML="Win  "+ b1.innerHTML;
+	turn.style.fontSize="40px";
+}
+
+function getWinner(){
+	let box1=document.getElementById("box1");
+	let box2=document.getElementById("box2");
+	let box3=document.getElementById("box3");
+	let box4=document.getElementById("box4");
+	let box5=document.getElementById("box5");
+	let box6=document.getElementById("box6");
+	let box7=document.getElementById("box7");
+	let box8=document.getElementById("box8");
+	let box9=document.getElementById("box9");
+
+	if (box1.innerHTML != "" && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML){
+		selectWinnerBoxes(box1,box2,box3);
+	}
+	if (box4.innerHTML != "" && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML){
+		selectWinnerBoxes(box4,box5,box6);
+	}
+	if (box7.innerHTML != "" && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML){
+		selectWinnerBoxes(box7,box8,box9);
+	}
+	if (box1.innerHTML != "" && box1.innerHTML === box4.innerHTML && box1.innerHTML === box7.innerHTML){
+		selectWinnerBoxes(box1,box4,box7);
+			
+	}	
+	if (box2.innerHTML != "" && box2.innerHTML === box5.innerHTML && box2.innerHTML === box8.innerHTML){
+		selectWinnerBoxes(box2,box5,box8);
+			
+	}		
+	if (box3.innerHTML != "" && box3.innerHTML === box6.innerHTML && box3.innerHTML === box9.innerHTML){
+		selectWinnerBoxes(box3,box6,box9);
+	}
+
+	if (box1.innerHTML != "" && box1.innerHTML === box5.innerHTML && box1.innerHTML === box9.innerHTML){
+		selectWinnerBoxes(box1,box5,box9);
+	}
+	if (box3.innerHTML != "" && box3.innerHTML === box5.innerHTML && box3.innerHTML === box7.innerHTML){
+		selectWinnerBoxes(box3,box5,box7);
+	}	
+}
+	
+
+	for (var i=0;i<boxes.length;i++){
+		boxes[i].onclick=function(){
+			if (this.innerHTML!="X" && this.innerHTML!=="O"){
+				if (xo%2===0) {
+					this.innerHTML="X";
+					turn.innerHTML="Turn O";
+					getWinner();
+					xo+=1;
+				}	
+				else{
+					this.innerHTML="O";
+					turn.innerHTML="Turn X";
+					getWinner();
+					xo+=1;
+				}
+			}	
+		};
+	}
+
+
+	function replay(){
+		for (var i=0;i<boxes.length;i++){
+			boxes[i].classList.remove("win");
+			boxes[i].innerHTML="";
+			turn.innerHTML="Play";
+			turn.style.fontSize="25px";
+
+		}	
+	}
+	
